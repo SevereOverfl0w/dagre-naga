540cebd55479076ac53c3e1184e1fc5c11df58b1
Dominic Monroe <dominic@juxt.pro>
2018-05-30 15:31:10 +0100

    Refactor into library shape
    - Fix label overlap
    - Move dom manipulation into a function
    - Separate axioms and rules from d3

--- src/my/app.cljs
+++ src/my/app.cljs
@@ -1,11 +1,8 @@
 (ns my.app
   (:require
-    ["dagre-d3" :as dagreD3]
-    ["d3" :as d3]
-    [clojure.string :as string]
+    [my.run :refer [run-graph!]]
     [naga.rules :as r :refer-macros [r]]
     [naga.engine :as e]
-    [naga.store :as store]
     [asami.core :as mem]))
 
 (def rules
@@ -21,65 +18,10 @@
   [[:fred :sibling :barney]
    [:fred :mother :mary]
    [:mary :sibling :george]
+   [:mary :gender :female]
    [:george :gender :male]])
 
 (def program (r/create-program rules axioms))
 (def store (first (e/run {:type :memory} program)))
 
-(def g
-  (.setGraph
-    (new (-> dagreD3
-             (.-graphlib)
-             (.-Graph))
-         #js {:multigraph true})
-    #js {}))
-
-(def entities (map first (store/query store '[?e] '[[?e ?p ?v]])))
-
-(doseq [state entities]
-  (.setNode g (name state) #js {:labelType "html"
-                                :label (str "<div><strong>" (name state) "</strong>"
-                                            ;; TODO: without negation, I can't figure out how to get ?v where ?v is not an ?e
-                                            (string/join
-                                              ","
-                                              (map
-                                                (fn [[p v]]
-                                                  (str "<div>" p " " v "</div>"))
-                                                (let [x (set entities)]
-                                                  (filter
-                                                    (fn [[_ v]]
-                                                      (not (contains? x v)))
-                                                    (store/query store
-                                                                 '[?p ?v]
-                                                                 [[state '?p '?v]])))))
-                                            "</div>")}))
-
-(doseq [[e a v] (store/query store '[?e ?a ?v] '[[?e ?a ?v]
-                                                 [?v ?discard ?discardx]])]
-  (prn e a v (str (name e) "-" (name v)))
-  (.setEdge g (name e) (name v) #js {:label (name a)}
-                                (str (name e) "-" (name a) "-" (name v))))
-
-(def svg (.select d3 "svg"))
-(def inner (.select svg "g"))
-(def zoom (.on (.zoom d3)
-               "zoom"
-               (fn []
-                 (.attr inner "transform" (.-transform (.-event d3))))))
-
-(.call svg zoom)
-
-(let [render (new (.-render dagreD3))]
-  (render inner g))
-
-(def initialScale 0.75)
-
-(.call svg (.-transform zoom) (-> (.-zoomIdentity d3)
-                                  (.translate
-                                    (* (- (.attr svg "width")
-                                          (.-width (.graph g)))
-                                       (/ initialScale 2))
-                                    20)
-                                  (.scale initialScale)))
-
-(.attr svg "height" (+ (* (.-height (.graph g)) initialScale) 40))
+(run-graph! store (js/document.querySelector "svg"))
---
+++ src/my/run.cljs
@@ -0,0 +1,57 @@
+(ns my.run
+  (:require
+    ["dagre-d3" :as dagreD3]
+    ["d3" :as d3]
+    [goog.object :as gobj]
+    [naga.store :as store]))
+
+(defn run-graph!
+  [store svg-node]
+  (let [g (.setGraph
+            (new (-> dagreD3
+                     (.-graphlib)
+                     (.-Graph))
+                 #js {:multigraph true})
+            #js {})
+        entities (map first (store/query store '[?e] '[[?e ?p ?v]]))]
+
+    (doseq [state entities]
+      (.setNode g (name state) #js {:labelType "html"
+                                    :label (str "<div><strong>" (name state) "</strong>"
+                                                ;; TODO: without negation, I can't figure out how to get ?v where ?v is not an ?e
+                                                (apply str
+                                                  (map
+                                                    (fn [[p v]]
+                                                      (str "<div>" p " " v "</div>"))
+                                                    (let [x (set entities)]
+                                                      (->> (store/query store
+                                                                        '[?p ?v]
+                                                                        [[state '?p '?v]])
+                                                           (filter (fn [[_ v]] (not (contains? x v))))
+                                                           (remove (comp #{:db/ident :db/id} first))))))
+                                                "</div>")}))
+
+    (doseq [[e a v] (store/query store '[?e ?a ?v] '[[?e ?a ?v]
+                                                     [?v ?discard ?discardx]])]
+      (.setEdge g (name e) (name v) #js {:label (name a)}
+                                    (str (name e) "-" (name a) "-" (name v))))
+
+    (gobj/set svg-node "innerHTML" "<g />")
+    (let [svg (.select d3 "svg")
+          inner (.select svg "g")
+          zoom (.on (.zoom d3) "zoom"
+                    (fn []
+                      (.attr inner "transform" (.-transform (.-event d3)))))]
+      (.call svg zoom)
+      (let [render (new (.-render dagreD3))
+            initialScale 0.75]
+        (render inner g)
+        (.call svg (.-transform zoom) (-> (.-zoomIdentity d3)
+                                          (.translate
+                                            (* (- (.attr svg "width")
+                                                  (.-width (.graph g)))
+                                               (/ initialScale 2))
+                                            20)
+                                          (.scale initialScale)))
+
+        (.attr svg "height" (+ (* (.-height (.graph g)) initialScale) 40))))))
